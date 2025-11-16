2117400c4b00a5d3b9ff6b8e32ba771612648f64
oaitbenh <0xmrrandom@gmail.com>
2025-11-16 07:42:13 +0100

    it's simple but it works

--- backend/src/main/java/blog/security/SecurityConfig.java
+++ backend/src/main/java/blog/security/SecurityConfig.java
@@ -3,11 +3,8 @@ package blog.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import blog.user.enums.Role;
@@ -24,17 +21,6 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .logout(logout -> logout.permitAll());
         return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-        UserDetails admin = org.springframework.security.core.userdetails.User
-                .builder()
-                .username("admin")
-                .password(passwordEncoder.encode("admin"))
-                .roles(Role.ADMIN.name())
-                .build();
-        return new InMemoryUserDetailsManager(admin);
     }
 
     @Bean
