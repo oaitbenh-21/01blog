11d739863aca5c3aa38b95f8af82fcae3b2179c7
oaitbenh-21 <0xoabh@gmail.com>
2025-11-17 18:51:30 +0100

    login and register

--- backend/src/main/java/blog/user/dto/SignResponse.java
+++ backend/src/main/java/blog/user/dto/SignResponse.java
@@ -1,12 +1,12 @@
 package blog.user.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-public class JwtResponse {
-    @NotBlank
+public class SignResponse {
     private String JwtToken;
+    private String Error;
+    private int StatusCode;
 }
