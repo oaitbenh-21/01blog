186db153c6417ba35c6202d778d9404e2c56aaff
oaitbenh <0xmrrandom@gmail.com>
2025-12-09 22:27:57 +0100

    update profile

--- src/main/java/blog/user/jpa/UserRepository.java
+++ src/main/java/blog/user/jpa/UserRepository.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     @Override
@@ -16,7 +19,12 @@ public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 
     List<UserEntity> findByFollowersGreaterThan(long followers);
 
-    // @Query("SELECT e FROM users e WHERE 1 > :var")
-    // List<EntityClass> CustomQuetyMethod(@Param("var") String value);
+    @Modifying
+    @Query(value = "UPDATE _users SET bio = :bio, fullname = :fullname, email = :email WHERE id = :uuid", nativeQuery = true)
+    void updateUser(@Param("uuid") UUID uuid, @Param("bio") String bio, @Param("fullname") String fullname,
+            @Param("email") String email);
+
+    @Query(value = "UPDATE _users SET avatar = :profile WHERE id = :uuid", nativeQuery = true)
+    void updateProfile(@Param("uuid") UUID uuid, @Param("profile") String profile);
 
 }
