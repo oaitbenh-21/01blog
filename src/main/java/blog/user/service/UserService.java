8beab41a475fd64eea90dfa427e1a68966056dbb
oaitbenh-21 <0xoabh@gmail.com>
2025-12-09 22:27:57 +0100

    update profile

--- src/main/java/blog/user/service/UserService.java
+++ src/main/java/blog/user/service/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import blog.user.dto.UpdateProfile;
 import blog.user.jpa.UserEntity;
 import blog.user.jpa.UserRepository;
 import jakarta.transaction.Transactional;
@@ -20,4 +21,13 @@ public class UserService implements UserDetailsService {
         return userRepo.findByUsername(Username).orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
     }
 
+    public void updateUser(UpdateProfile updateProfile, UserEntity user) {
+        // validate and update profile picture if provided
+        // update avatar
+        if (updateProfile.getAvatar() != null && !updateProfile.getAvatar().isEmpty()) {
+            // logic to store the avatar file and get its path or URL
+        }
+        userRepo.updateUser(user.getId(), updateProfile.getBio(), updateProfile.getFullname(),
+                updateProfile.getEmail());
+    }
 }
