cc8e540b8b7a6f68c872969a7d8e7c7e3053beb3
oaitbenh-21 <0xoabh@gmail.com>
2025-12-13 00:47:39 +0100

    handle exception in auth functions

--- src/main/java/blog/auth/service/AuthenticationService.java
+++ src/main/java/blog/auth/service/AuthenticationService.java
@@ -1,5 +1,6 @@
 package blog.auth.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,6 +13,7 @@ import blog.security.jwt.JwtService;
 import blog.user.jpa.UserEntity;
 import blog.user.jpa.UserRepository;
 import blog.user.jpa.utils.Role;
+import io.jsonwebtoken.io.IOException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -22,26 +24,39 @@ public class AuthenticationService {
     private final AuthenticationManager authManager;
     private final JwtService jwtService;
 
-    public void register(RegistrationRequest request) {
-        var user = UserEntity.builder()
-                .fullname(request.getFullname())
-                .username(request.getUsername())
-                .password(passwdEncoder.encode(request.getPassword()))
-                .email(request.getEmail())
-                .role(Role.USER)
-                .build();
-        repository.save(user);
+    public AuthenticateResponse register(RegistrationRequest request) {
+        AuthenticateResponse response;
+        try {
+            var user = UserEntity.builder()
+                    .fullname(request.getFullname())
+                    .username(request.getUsername())
+                    .password(passwdEncoder.encode(request.getPassword()))
+                    .email(request.getEmail())
+                    .role(Role.USER)
+                    .build();
+            repository.save(user);
+            response = AuthenticateResponse.builder().status(HttpStatus.CREATED).token(jwtService.generateToken(user))
+                    .build();
+        } catch (Exception e) {
+            response = AuthenticateResponse.builder().status(HttpStatus.BAD_REQUEST).error(e.getMessage()).build();
+        }
+        return response;
     }
 
     public AuthenticateResponse authenticate(AuthenticateRequest req) {
-        var auth = authManager.authenticate(
-                new UsernamePasswordAuthenticationToken(req.getUsername(), req.getPassword()));
-        var user = ((UserEntity) auth.getPrincipal());
-        var jwtToken = jwtService.generateToken(user);
-
-        return AuthenticateResponse.builder()
-                .token(jwtToken)
-                .build();
+        AuthenticateResponse response;
+        try {
+            var auth = authManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(
+                            req.getUsername(),
+                            req.getPassword()));
+            var user = ((UserEntity) auth.getPrincipal());
+            var jwtToken = jwtService.generateToken(user);
+            response = AuthenticateResponse.builder().status(HttpStatus.OK).token(jwtToken).build();
+        } catch (IOException e) {
+            response = AuthenticateResponse.builder().status(HttpStatus.BAD_REQUEST).error(e.getMessage()).build();
+        }
+        return response;
     }
 
 }
