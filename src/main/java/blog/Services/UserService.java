03da44a58d95f26f6d11fdd8fe8a5a9082322430
oaitbenh-21 <0xoabh@gmail.com>
2026-01-15 12:25:58 +0100

    add follow unfollow logic

--- src/main/java/blog/Services/UserService.java
+++ src/main/java/blog/Services/UserService.java
@@ -10,6 +10,7 @@ import blog.Repositories.SubscriptionRepository;
 import blog.Repositories.UserRepository;
 import blog.Dto.UserDto;
 import blog.Model.Post;
+import blog.Model.Subscription;
 
 import java.util.List;
 
@@ -72,20 +73,20 @@ public class UserService {
         return false;
     }
 
-    // public void subscribe(Long id) {
-    // User current = getCurrentUser();
-    // User toFollow = getUserById(id);
-    // if (subscriptionRepository.existsByFollowerAndFollowing(current, toFollow))
-    // throw new RuntimeException("Already subscribed");
-    // subscriptionRepository.save(new Subscription(null, current, toFollow, null));
-    // }
+    public void subscribe(Long id) {
+        User current = getCurrentUser();
+        User toFollow = getUserById(id);
+        if (subscriptionRepository.existsByFollowerAndFollowing(current, toFollow))
+            throw new RuntimeException("Already subscribed");
+        subscriptionRepository.save(new Subscription(null, current, toFollow, null));
+    }
 
-    // public void unsubscribe(Long id) {
-    // User current = getCurrentUser();
-    // User toUnfollow = getUserById(id);
-    // subscriptionRepository.findByFollowerAndFollowing(current, toUnfollow)
-    // .ifPresent(subscriptionRepository::delete);
-    // }
+    public void unsubscribe(Long id) {
+        User current = getCurrentUser();
+        User toUnfollow = getUserById(id);
+        subscriptionRepository.findByFollowerAndFollowing(current, toUnfollow)
+                .ifPresent(subscriptionRepository::delete);
+    }
 
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
