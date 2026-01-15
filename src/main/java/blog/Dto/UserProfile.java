985b0ef9760c71b69056313db99b003602f71868
oaitbenh-21 <0xmrrandom@gmail.com>
2026-01-15 12:05:58 +0100

    add check is this my profile

--- src/main/java/blog/Dto/UserProfile.java
+++ src/main/java/blog/Dto/UserProfile.java
@@ -14,9 +14,11 @@ public class UserProfile {
     private String email;
     private String bio;
     private String role;
+    private boolean follow;
+    private boolean isMine;
     private List<PostResponseDto> posts;
 
-    public static UserProfile from(User user, List<PostResponseDto> posts) {
+    public static UserProfile from(User user, List<PostResponseDto> posts, boolean follow, boolean isMine) {
         return UserProfile.builder()
                 .id(user.getId())
                 .username(user.getUsername())
@@ -24,6 +26,8 @@ public class UserProfile {
                 .bio(user.getBio())
                 .role(user.getRole().name())
                 .posts(posts)
+                .follow(follow)
+                .isMine(isMine)
                 .build();
     }
 
