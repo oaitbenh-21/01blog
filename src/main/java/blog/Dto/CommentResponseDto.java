a19b24c94e23b5961fd84d20d87064ad706c0f45
omar ait benhammou <0xmrrandom@gmail.com>
2026-02-05 01:31:11 +0100

    comments id to delete them

--- src/main/java/blog/Dto/CommentResponseDto.java
+++ src/main/java/blog/Dto/CommentResponseDto.java
@@ -7,11 +7,12 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class CommentResponseDto {
+    private Long id;
     private String content;
     private PostAuthor author;
     private boolean mine;
 
     public static CommentResponseDto from(Comment comment, boolean mine) {
-        return new CommentResponseDto(comment.getContent(), PostAuthor.from(comment.getUser()), mine);
+        return new CommentResponseDto(comment.getId(), comment.getContent(), PostAuthor.from(comment.getUser()), mine);
     }
 }
